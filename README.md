ef9352d0136044f0c2d3925adc9d000252040534
Mongkol7 <thoeungsereymongkol@gmail.com>
2026-08-15 15:56:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Ecom_SpringBoot_SV2.6" 
---
+++ src/main/java/com/example/springboot_midterm/SpringBootMidTermApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMidTermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMidTermApplication.class, args);
+	}
+
+}
